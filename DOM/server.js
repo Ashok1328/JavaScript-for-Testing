8f40bdb67dcf209158c9fdcf22f9e229d7abf9d6
Ashok1328 <ashokkhatiwada1990@gmail.com>
2026-01-22 14:40:02 +0545

    DOM with events

--- DOM/server.js
+++ DOM/server.js
@@ -34,3 +34,20 @@ document.querySelector("body").before(newBtn);
 
 let content = document.querySelector("p");
 content.classList.add("newClass");
+
+// -----------------------------------------------
+let modeBtn = document.querySelector("#mode");
+let currMode = "light";  //dark
+modeBtn.addEventListener("click", ()=>
+{
+  if(currMode === "light")
+  {
+    currMode = "dark";
+    document.querySelector("body").style.backgroundColor = "black";
+  }
+  else
+  {
+    currMode = "light";
+    document.querySelector("body").style.backgroundColor = "white";
+  }
+})
